32a9236303a8a33580cb8fb9b4629290cbc2f7a4
Manish-Lalwani <54809501+Manish-Lalwani@users.noreply.github.com>
2019-12-26 04:17:03 +0530

    Update simple_stack.py

--- Stack/simple_stack.py
+++ Stack/simple_stack.py
@@ -1,33 +1,62 @@
 #Stack using list
 
+class MyQueue:
 
-my_stack =[]
+	def __init__(self):
+		self.my_stack =[]
 
-my_stack.append(10)
-my_stack.append(20)
-my_stack.append(30)
-my_stack.append(40)
-my_stack.append(50)
-my_stack.append(60)
+
+	def push(self,data):
+		self.my_stack.append(data)
+
+
+	def pop(self):
+		return self.my_stack.pop()
+
+
+	def isempty(self):
+		if len(my_stack) == 0:
+			print("Stack is Empty ")
+		else:
+			print("Stack is not Empty")
+
+
+	def display(self):
+		print(self.my_stack)	
+
+
+
+
+#################################################
+mq1 = MyQueue()
+
+mq1.push(10)
+mq1.push(20)
+mq1.push(30)
+mq1.push(40)
+mq1.push(50)
+mq1.push(60)
 
 
 #Displaying elements in stack
 print("=================================================")
 print("Printing Elements after Inserting in queue ")
-print(my_stack)
+mq1.display()
 
 
 #Removing or popping element
-element = my_stack.pop()
+element = mq1.pop()
 print("=================================================")
 print("Element removed is : {} ".format(element))
-print("Printing elements in queue after removal :\n",my_stack)
+print("Printing elements in queue after removal :\n")
+mq1.display()
 
 #Again pushing/appending an element
-my_stack.append(70)
+mq1.push(70)
 print("=================================================")
 print("Printing Elements after Inserting in queue ")
-print(my_stack)
+mq1.display()
+
 
 
 
